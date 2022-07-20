c289244f82fd7b92807af8ff1f5d7fd72315f66e
Jade Ferguson <jadesnider2440@gmail.com>
2022-07-20 06:47:52 -0700

    Test class

--- DieRoller/Program.cs
+++ DieRoller/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using DieRoller;
+
+Die myDie = new Die(); // When a die is created, a random FaceValue should be set
+Console.WriteLine(myDie.FaceValue);
+myDie.Roll();
+Console.WriteLine(myDie.FaceValue);
+myDie.Roll();
+Console.WriteLine(myDie.FaceValue);
+myDie.Roll();
+Console.WriteLine(myDie.FaceValue);
+myDie.Roll();
+Console.WriteLine(myDie.FaceValue);
+
+myDie.IsHeld = true;
+Console.WriteLine("Holding the die*******");
+myDie.Roll();
+Console.WriteLine(myDie.FaceValue);
+myDie.Roll();
+Console.WriteLine(myDie.FaceValue);
+
+Console.ReadKey();
